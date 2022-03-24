207eb7b1725a0846e4b5dab324bf03f4d3165b22
Devon <grandthefauto77@gmail.com>
2022-03-24 12:29:04 +0700

    fixing bugs

--- dbReadSiswaKelas.php
+++ dbReadSiswaKelas.php
@@ -1,8 +1,10 @@
 <?php
 require_once('dbConnect.php');
-if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-  $sql = "SELECT * FROM siswa s INNER JOIN kelas k ON s.id_kelas = k.id_kelas ORDER BY k.nama_kelas ASC";
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
+  $id_kelas = $_POST['id_kelas'];
+
+  $sql = "SELECT * FROM siswa s INNER JOIN kelas k ON s.id_kelas = k.id_kelas WHERE s.id_kelas ='$id_kelas' ORDER BY k.nama_kelas ASC";
   $result = array();
   $res = mysqli_query($con, $sql);
   $res2 = mysqli_query($con, $sql);
