32da27bd1caf68e991e283414ae86d4a45f133c2
Devon <grandthefauto77@gmail.com>
2022-03-27 16:20:40 +0700

    Update message pembayaran

--- dbUpdatePembayaran.php
+++ dbUpdatePembayaran.php
@@ -17,48 +17,47 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         $sql_b = "UPDATE pembayaran SET tgl_bayar = now(), jumlah_bayar = '$nominal', id_petugas = '$id_petugas', status_bayar = 'Lunas', kurang_bayar = 0 WHERE id_pembayaran = '$id_pembayaran'";
         if (mysqli_query($con, $sql_b)) {
             $response["value"] = 1;
-            $response["message"] = "Sukses update data!";
+            $response["message"] = "SPP Lunas!";
             echo json_encode($response);
         } else {
             $response["value"] = 0;
-            $response["message"] = "Gagal update data lunas!";
+            $response["message"] = "Gagal update, silahkan coba lagi...";
             echo json_encode($response);
         }
     } else if ($jumlah_bayar > 0 && $jumlah_bayar < $nominal && $kurang_bayar == 0) {
         $sql_c = "UPDATE pembayaran SET tgl_bayar = now(), jumlah_bayar = '$jumlah_bayar', id_petugas = '$id_petugas', status_bayar = 'Belum', kurang_bayar = $nominal-$jumlah_bayar WHERE id_pembayaran = '$id_pembayaran'";
         if (mysqli_query($con, $sql_c)) {
             $response["value"] = 1;
-            $response["message"] = "Sukses update data!";
+            $response["message"] = "SPP masih kurang!";
             echo json_encode($response);
         } else {
             $response["value"] = 0;
-            $response["message"] = "Gagal update data kurang awal!";
+            $response["message"] = "Gagal update, silahkan coba lagi...";
             echo json_encode($response);
         }
     } else if ($jumlah_bayar > 0 && $jumlah_bayar < $nominal && $kurang_bayar > 0) {
         $sql_d = "UPDATE pembayaran SET tgl_bayar = now(), jumlah_bayar = jumlah_bayar+'$jumlah_bayar', id_petugas = '$id_petugas', status_bayar = 'Belum', kurang_bayar = kurang_bayar-$jumlah_bayar WHERE id_pembayaran = '$id_pembayaran'";
         mysqli_query($con, $sql_d);
-        $check2 = mysqli_fetch_array(mysqli_query($con, $sql_a));
-        $kurangnya = $check2['kurang_bayar'];
+        $kurangnya = $check['kurang_bayar'];
 
         if ($kurangnya == 0) {
             $sql_f = "UPDATE pembayaran SET tgl_bayar = now(), jumlah_bayar = '$nominal', id_petugas = '$id_petugas', status_bayar = 'Lunas' WHERE id_pembayaran = '$id_pembayaran'";
             if (mysqli_query($con, $sql_f)) {
                 $response["value"] = 1;
-                $response["message"] = "Sukses update data!";
+                $response["message"] = "SPP Lunas!";
                 echo json_encode($response);
             } else {
                 $response["value"] = 0;
-                $response["message"] = "Gagal update kurang lanjut!";
+                $response["message"] = "Gagal update, silahkan coba lagi...";
                 echo json_encode($response);
             }
         } else if ($kurangnya > 0) {
             $response["value"] = 1;
-            $response["message"] = "Sukses update data!";
+            $response["message"] = "SPP masih kurang!";
             echo json_encode($response);
         } else {
             $response["value"] = 0;
-            $response["message"] = "Gagal update kurang lanjut!";
+            $response["message"] = "Gagal update, silahkan coba lagi...";
             echo json_encode($response);
         }
 
