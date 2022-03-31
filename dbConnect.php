158118a98183b1891b719fb8ce109fb1edbd619f
LethanAxeger <53499521+lethanaxeger@users.noreply.github.com>
2022-03-31 22:09:53 +0700

    Changed DB Connect

--- dbConnect.php
+++ dbConnect.php
@@ -2,7 +2,7 @@
 define('HOST', 'localhost');
 define('USER', 'root');
 define('PASS', '');
-define('DB', 'spp');
+define('DB', 'espepe');
 
 $con = mysqli_connect(HOST, USER, PASS, DB) or die('Unable to Connect');
 
